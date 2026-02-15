e984db4c192af6bb4c415aa2693ddf1947dd39b8
Eric Buenavida Crespo <e.buenavida06@gmail.com>
2026-02-15 23:28:53 +0100

    Add files via upload

---
+++ src/VideoGameInfo.java
@@ -0,0 +1,83 @@
+public class VideoGameInfo {
+
+    private String title;
+    private String series;
+    private String publisher;
+    private  short year;
+    private    int sales;
+
+    private static final int     TITLE_LIMIT = 24;
+    private static final int    SERIES_LIMIT = 19;
+    private static final int PUBLISHER_LIMIT = 18;
+
+    public  static final int SIZE = 2*TITLE_LIMIT + 2*SERIES_LIMIT + 2*PUBLISHER_LIMIT + 2 + 4;
+
+    public VideoGameInfo (String title, String series, String publisher, short year, int sales) {
+        this.title     = title;
+        this.series    = series;
+        this.publisher = publisher;
+        this.year      = year;
+        this.sales     = sales;
+    }
+
+    // Getters
+    public String getTitle     () { return title;     }
+    public String getSeries    () { return series;    }
+    public String getPublisher () { return publisher; }
+    public  short getYear      () { return year;      }
+    public    int getSales     () { return sales;     }
+
+    public byte[] toBytes() {
+        byte[] record = new byte[SIZE];
+        int offset = 0;
+        PackUtils.packString(title, TITLE_LIMIT, record, offset);
+        offset += 2*TITLE_LIMIT;
+        PackUtils.packString(series, SERIES_LIMIT, record, offset);
+        offset += 2*SERIES_LIMIT;
+        PackUtils.packString(publisher, PUBLISHER_LIMIT, record, offset);
+        offset += 2*PUBLISHER_LIMIT;
+        PackUtils.packShort(year, record, offset);
+        offset += 2;
+        PackUtils.packInt(sales, record, offset);
+        //offset += 4
+        return record;
+    }
+
+    public static VideoGameInfo fromBytes (byte[] record) {
+        int offset = 0;
+        String title = PackUtils.unpackString(TITLE_LIMIT, record, offset);
+        offset += 2*TITLE_LIMIT;
+        String series = PackUtils.unpackString(SERIES_LIMIT, record, offset);
+        offset += 2*SERIES_LIMIT;
+        String publisher = PackUtils.unpackString(PUBLISHER_LIMIT, record, offset);
+        offset += 2*PUBLISHER_LIMIT;
+        short year = PackUtils.unpackShort(record, offset);
+        offset += 2;
+        int sales = PackUtils.unpackInt(record, offset);
+        //offset += 4
+        return new VideoGameInfo(title, series, publisher, year, sales);
+    }
+
+    public String toString() {
+        String ls = System.lineSeparator();
+        String result = title;
+        if (!series.isEmpty()) {
+            result += " (sèrie " + series + ")";
+        }
+        if (!publisher.isEmpty() || year >= 0) {
+            result += ls + "Publicat";
+            if (!publisher.isEmpty()) {
+                result += " per " + publisher;
+            }
+            if (year >= 0) {
+                result += " en l'any " + year;
+            }
+            result += ".";
+        }
+        if (sales >= 0) {
+            result += ls + "Ha venut aproximadament " + sales + " còpies.";
+        }
+        return result;
+    }
+
+}
